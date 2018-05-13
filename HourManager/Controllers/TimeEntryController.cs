f061f31340436154e38ccbfa3a87dbeefd30d67a
daydraze <spyte@outlook.com>
2018-05-13 22:55:42 +0200

    Add project files.

---
+++ HourManager/Controllers/TimeEntryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HourManager.Controllers
+{
+    public class TimeEntryController : Controller
+    {
+        // GET: TimeEntry
+        public ActionResult Index()
+        {
+            return View();
+            
+        }
+    }
+}
---
+++ HourManager/Models/TimeEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HourManager.Models
+{
+    public class TimeEntry
+    {
+        public string Title { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
